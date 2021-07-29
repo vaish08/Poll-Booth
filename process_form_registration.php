65e5d5a3ce606ea53b2a7678a903ad73d01b1e1f
Vaishnavi B <vaishnavib@MacBook-Air.local>
2021-07-29 22:20:00 +0530

    Registration and Login

---
+++ process_form_registration.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Registration</title>
+</head>
+<body>
+<?php
+    require('db.php');
+    // When form submitted, insert values into the database.
+    if (isset($_REQUEST['submit'])) {
+
+        $username = ($_REQUEST['username']);
+        $username = mysqli_real_escape_string($con, $username);
+
+
+        $password = ($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+
+
+        $sql = "SELECT * from `login` where (username = '$username'); ";
+        $res = mysqli_query($con, $sql);
+
+        if(mysqli_num_rows($res) > 0){
+          $message = "Username Already exits.";
+        }
+        else{
+        $query    = "INSERT into `login` (username, password)
+                     VALUES ('$username', '" . md5($password) . "')";
+        $result   = mysqli_query($con, $query);
+
+        if ($result) {
+            echo "<div class='form'>
+                  <h3>You are registered successfully.</h3><br/>";
+        } else {
+            echo "<div class='form'>
+                  <h3>Required fields are missing.</h3><br/>
+                  <p class='link'>Click here to <a href='registration.php'>register</a> again.</p>
+                  </div>";
+        }
+    }}
+?>
+</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form class="form" action="" method="post">
+        <h1 class="login-title">Registration</h1>
+        <input type="text" class="login-input" name="username" placeholder="Username" required />
+        <input type="password" class="login-input" name="password" placeholder="Password">
+        <input type="submit" name="submit" value="Register" class="login-button">
+        <p class="link"><a href="login.php">Click to Login</a></p>
+    </form>
+
+    <?php
+      if(isset($_REQUEST['username'])){
+        require("process_form_registration.php");
+        echo "<div>$message</div>";
+      }
+     ?>
+  </body>
+</html>
