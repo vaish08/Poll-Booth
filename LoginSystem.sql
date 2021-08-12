a84fcbbb70993c59f77af72b3a529bef2da8c679
vaish08 <81669927+vaish08@users.noreply.github.com>
2021-08-12 20:00:33 +0530

    Update LoginSystem.sql

--- LoginSystem.sql
+++ LoginSystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Aug 08, 2021 at 05:41 AM
+-- Generation Time: Aug 12, 2021 at 02:19 PM
 -- Server version: 5.7.32
 -- PHP Version: 7.4.16
 
@@ -39,13 +39,9 @@ CREATE TABLE `login` (
 
 INSERT INTO `login` (`id`, `username`, `password`) VALUES
 (1, 'vaish', '81dc9bdb52d04dc20036dbd8313ed055'),
-(20, 'sad\'sad', '81dc9bdb52d04dc20036dbd8313ed055'),
-(21, 'asd\'asd', '81dc9bdb52d04dc20036dbd8313ed055'),
-(22, 'hi\'hi', '81dc9bdb52d04dc20036dbd8313ed055'),
 (23, 'jenny', '81dc9bdb52d04dc20036dbd8313ed055'),
-(24, 'hi', '81dc9bdb52d04dc20036dbd8313ed055'),
-(25, 'dad', '81dc9bdb52d04dc20036dbd8313ed055'),
-(26, 'heybuddy', '81dc9bdb52d04dc20036dbd8313ed055');
+(27, 'ram123', '81dc9bdb52d04dc20036dbd8313ed055'),
+(28, 'sarah', '81dc9bdb52d04dc20036dbd8313ed055');
 
 -- --------------------------------------------------------
 
@@ -66,18 +62,8 @@ CREATE TABLE `polls` (
 --
 
 INSERT INTO `polls` (`id`, `photo`, `title`, `description`, `deadline`) VALUES
-(60, '05_05_2021.jpg', 'Delta t-shirt', 'Choose the following answer', ''),
-(61, '29_05_2021.jpg', 'test-1', 'Choose the following answer', '08/07/2002'),
-(62, '05_05_2021.jpg', 'test-2', 'Choose the following answer', '14/12/2021'),
-(63, '05_05_2021.jpg', 'test-3', 'Choose the following answer', '-Nil-'),
-(64, '27_04_2021.jpg', 'test-4', 'Choose the following answer', '02/08/2021'),
-(65, '29_05_2021.jpg', 'test-5', 'Choose the following answer', '02/08/2021 23:59'),
-(66, '27_04_2021.jpg', 'test-5', 'Choose the following answer', '02/08/2021 19:06'),
-(67, '27_04_2021.jpg', 'test-6', 'Choose the following answer', '02/08/2021 23:59'),
-(68, '05_05_2021.jpg', 'test-7', 'Choose the following answer', '02/08/2021 23:59'),
-(69, '29_05_2021.jpg', 'test-7', 'Choose the following answer', '03/08/2021 16:25'),
-(70, '', 'test-8', '', ''),
-(71, '05_05_2021.jpg', 'test-9', 'choose', '');
+(72, 'shirt-1.jpeg', 'Delta t-shirt', 'Do you agree for the following design ?', '-Nil-'),
+(73, 'shirt-2.jpeg', 'Delta t-shirt', 'Another option of shirt', '13/08/2021 23:59');
 
 -- --------------------------------------------------------
 
@@ -97,30 +83,12 @@ CREATE TABLE `poll_answers` (
 --
 
 INSERT INTO `poll_answers` (`id`, `poll_id`, `title`, `votes`) VALUES
-(62, 60, 'yes\r', 1),
-(63, 60, 'no', 0),
-(66, 61, 'Yes\r', 1),
-(67, 61, 'No', 0),
-(68, 62, 'Yes\r', 1),
-(69, 62, 'No', 0),
-(70, 63, 'Yes\r', 0),
-(71, 63, 'No', 0),
-(72, 64, 'Yes\r', 1),
-(73, 64, 'No', 0),
-(74, 65, 'Yes\r', 0),
-(75, 65, 'No', 0),
-(76, 66, 'Yes\r', 0),
-(77, 66, 'No', 0),
-(78, 67, 'Yes\r', 1),
-(79, 67, 'No', 0),
-(80, 68, 'Yes\r', 0),
-(81, 68, 'No', 0),
-(82, 69, 'Yes\r', 0),
-(83, 69, 'No', 0),
-(84, 70, 'Yes\r', 2),
-(85, 70, 'No', 0),
-(86, 71, 'yes\r', 1),
-(87, 71, 'no', 0);
+(88, 72, 'Yes\r', 2),
+(89, 72, 'No\r', 1),
+(90, 72, 'Maybe the colour can be changed\r', 0),
+(91, 73, 'Yes\r', 0),
+(92, 73, 'No\r', 2),
+(93, 73, 'Colour can be changed', 0);
 
 -- --------------------------------------------------------
 
@@ -134,6 +102,13 @@ CREATE TABLE `result` (
   `status` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `result`
+--
+
+INSERT INTO `result` (`id`, `poll_id`, `status`) VALUES
+(1, 72, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -146,6 +121,23 @@ CREATE TABLE `voted` (
   `user` text NOT NULL,
   `status` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `voted`
+--
+
+INSERT INTO `voted` (`id`, `poll_id`, `user`, `status`) VALUES
+(1, 60, 'jenny', 1),
+(2, 72, 'jenny', 1),
+(3, 72, 'jenny', 1),
+(4, 72, 'ram123', 1),
+(5, 72, 'ram123', 1),
+(6, 72, 'sarah', 1),
+(7, 72, 'sarah', 1),
+(8, 73, 'jenny', 1),
+(9, 73, 'jenny', 1),
+(10, 73, 'ram123', 1),
+(11, 73, 'ram123', 1);
 
 --
 -- Indexes for dumped tables
@@ -189,31 +181,31 @@ ALTER TABLE `voted`
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `polls`
 --
 ALTER TABLE `polls`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
 -- AUTO_INCREMENT for table `poll_answers`
 --
 ALTER TABLE `poll_answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 
 --
 -- AUTO_INCREMENT for table `result`
 --
 ALTER TABLE `result`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `voted`
 --
 ALTER TABLE `voted`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
